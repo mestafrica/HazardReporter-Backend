547868d343c9c0ca26499412c88c21395c43e53c
Huda Ohememwaa <asantehuda@gmail.com>
2026-04-15 23:42:23 +0000

    fix: allow latitude, longitude and location in hazard report validation

--- backend/src/validators/hazardreport.ts
+++ backend/src/validators/hazardreport.ts
@@ -6,6 +6,8 @@ export const hazardreportValidator = Joi.object({
     description: Joi.string().required(),
     images: Joi.array().items(Joi.string()).sparse(false).default([]),
     location: Joi.string().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
     city: Joi.string().required(),
     country: Joi.string().required(),
     user: Joi.string().optional()
